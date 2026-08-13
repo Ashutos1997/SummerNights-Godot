f64d90d29c150c3cc472704f2a2dea1e7310d7f6
Ash J <ashutos1997@gmail.com>
2026-08-13 19:16:12 +0530

    Fix ambient audio not looping on title screen

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -279,6 +279,7 @@ func _ready() -> void:
 	ambient_sfx.volume_db = -80.0  # Start silent, fade in below
 	add_child(ambient_sfx)
 	ambient_sfx.play()
+	ambient_sfx.finished.connect(func(): ambient_sfx.play())
 	# Fade in ocean ambient over 2s on title screen
 	var ocean_tw = create_tween()
 	ocean_tw.tween_property(ambient_sfx, "volume_db", -8.0, 2.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
