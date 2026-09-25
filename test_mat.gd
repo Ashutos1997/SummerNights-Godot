f86f4ebdf8ab601510635bd2076a3061f2457be2
Ash J <ashutos1997@gmail.com>
2026-09-25 23:34:23 +0530

    chore: Clean up legacy test scripts and UID files from project root

--- test_mat.gd
+++
@@ -1,6 +0,0 @@
-extends SceneTree
-func _init():
-    var m = StandardMaterial3D.new()
-    print("specular in m? ", "specular" in m)
-    print("metallic_specular in m? ", "metallic_specular" in m)
-    quit()
